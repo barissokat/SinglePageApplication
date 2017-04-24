28124accaeb5c56413562953465d58a12750bdab
bsokat <barissokat@gmail.com>
2017-04-24 17:37:39 +0300

    Adding home controller and view, and configuring routes

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 var app = angular.module('bookStoreApp', ['ngRoute']);
 app.config(function ($routeProvider) {
 
+	$routeProvider.when("/home", {
+		controller: "homeController",
+		templateUrl: "app/views/home.html"
+	});
+
 	$routeProvider.when("/books", {
 		controller: "booksController",
 		templateUrl: "app/views/books.html"
 	});
-
-	$routeProvider.otherwise({ redirectTo: "/books"});
 
 });
