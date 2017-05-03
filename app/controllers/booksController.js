9a02fba6dea9ba1381c2e24834c2aad9d56d2223
bsokat <barissokat@gmail.com>
2017-05-03 09:43:06 +0300

    Removed unuseable things

--- app/controllers/booksController.js
+++ app/controllers/booksController.js
@@ -2,13 +2,4 @@ app.controller('booksController', ['$scope', 'bookService', function ($scope, bo
 	bookService.getResponders.then(function (data) {
 		$scope.booksList = data;
 	});
-
-	$scope.plusOne = function (index) {
-		$scope.books[index].likes += 1;
-	};
-
-	$scope.minusOne = function (index) {
-		$scope.books[index].dislikes += 1;
-	};
-
 }]);
