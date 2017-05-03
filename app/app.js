fbf63c6d863f4328243ba47da7c782dbb31376ba
bsokat <barissokat@gmail.com>
2017-05-03 08:56:40 +0300

    Fixed AngularJs routing

--- app/app.js
+++ app/app.js
@@ -1,16 +1,22 @@
 var app = angular.module('bookStoreApp', ['ngRoute']);
+app.config(['$locationProvider', function($locationProvider) {
+  	$locationProvider.hashPrefix('');
+}]);
 app.config(function ($routeProvider) {
 
-	$routeProvider.when("/home", {
+	$routeProvider
+	.when("/home", {
 		controller: "homeController",
 		templateUrl: "app/views/home.html"
-	});
+	})
 
-	$routeProvider.when("/books", {
+	.when("/books", {
 		controller: "booksController",
 		templateUrl: "app/views/books.html"
-	});
+	})
 
-	$routeProvider.otherwise({ redirectTo: "/home"});
+	.otherwise({ redirectTo: "/home"});
 
+	// configure html5 to get links working on jsfiddle
+  	// $locationProvider.html5Mode(true);
 });
