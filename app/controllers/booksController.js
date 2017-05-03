2db63b9a40065cdd7e9005941a6e557fda219dc7
bsokat <barissokat@gmail.com>
2017-05-03 09:25:34 +0300

    Modify controller for using with service

--- app/controllers/booksController.js
+++ app/controllers/booksController.js
@@ -1,42 +1,7 @@
-app.controller('booksController', ['$scope', function ($scope) {
-	$scope.books = [
-		{
-			cover: 'content/images/books.jpg',
-			title: 'Hackers & Painters',
-			author: 'Paul Graham',
-			price: '10',
-			year: '2007',
-      		likes: 0,
-      		dislikes: 0
-		},
-		{
-			cover: 'content/images/books.jpg',
-			title: 'The Art Of Intrusion',
-			author: 'Kevin D. Mitnick',
-			price: '12',
-			year: '2005',
-      		likes: 0,
-      		dislikes: 0
-		},
-		{
-			cover: 'content/images/books.jpg',
-			title: 'Waltzing with Bears',
-			author: 'Tom DeMarco',
-			price: '15',
-			year: '2003',
-      		likes: 0,
-      		dislikes: 0
-		},
-		{
-			cover: 'content/images/books.jpg',
-			title: 'Dreaming in Code',
-			author: 'Scott Rosenberg',
-			price: '17',
-			year: '2008',
-      		likes: 0,
-      		dislikes: 0
-		}
-	];
+app.controller('booksController', ['$scope', 'bookService', function ($scope, bookService) {
+	bookService.getResponders.then(function (data) {
+		$scope.booksList = data;
+	});
 
 	$scope.plusOne = function (index) {
 		$scope.books[index].likes += 1;
